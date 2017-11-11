f4139677f71d10ab68135f4a0466043e17978fbe
Michael MacFadden <michael@convergencelabs.com>
2017-11-10 16:27:59 -0800

    Updated compose file path.

--- __jenkins/up.groovy
+++ __jenkins/up.groovy
@@ -12,10 +12,10 @@ node {
     docker.withServer(env.ConvergenceIOWebHost, 'ConvergenceIOWebDocker') {
       docker.withRegistry('https://nexus.convergencelabs.tech:18443/', 'NexusRepo') {
         stage 'Pull'
-        sh 'docker-compose pull'
+        sh 'docker-compose -f __docker/docker-compose.yml pull'
 
         stage 'Up'
-        sh 'docker-compose -p convergence-io up -d'
+        sh 'docker-compose -f __docker/docker-compose.yml -p convergence-io up -d'
       }
     }
   }
