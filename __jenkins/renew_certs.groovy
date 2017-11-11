1aeccff01a54f60aca2abf7b6cd4ee8cfabbb5f1
Michael MacFadden <michael@convergencelabs.com>
2017-11-10 17:50:03 -0800

    Finished cert renewal.

--- __jenkins/renew_certs.groovy
+++ __jenkins/renew_certs.groovy
@@ -13,28 +13,27 @@ node {
       stage 'Renew Certs'
       sh '''
       docker run --rm \
+        --user $(id -u) \
         --name certbot \
         --volume=/home/rancher/letsencrypt_certs/:/etc/letsencrypt/ \
-        -p 443:443 \
+        --volume=/home/rancher/letsencrypt_acme:/var/www \
         certbot/certbot \
         certonly \
-        --standalone \
+        --authenticator webroot \
+        --webroot-path /var/www  \
+        --server https://acme-v01.api.letsencrypt.org/directory \
+        --renew-by-default \
         --agree-tos \
         --non-interactive \
         -m sysadmin@convergencelabs.com \
         -d convergence.io -d www.convergence.io
-
-      docker run -i --rm --name letsencrypt \
-           --volume=/home/rancher/letsencrypt_certs/:/etc/letsencrypt/ \
-           --volume=/home/rancher/letsencrypt_www:/var/www \
-           certbot/certbot auth --authenticator webroot --webroot-path /var/www --renew-by-default --server \
-           https://acme-v01.api.letsencrypt.org/directory -d my.example.com
       '''
 
       stage 'Copy Certs'
       sh '''
-      cp /home/rancher/letsencrypt_certs/live/convergence.io/privkey.pem /home/rancher/ssl/server.key
-      cp /home/rancher/letsencrypt_certs/live/convergence.io/fullchain.pem /home/rancher/ssl/server.crt
+      sudo cp -a `readlink /home/rancher/letsencrypt_certs/live/convergence.io/fullchain.pem` /home/rancher/ssl/server.crt
+      sudo cp -a `readlink /home/rancher/letsencrypt_certs/live/convergence.io/privkey.pem` /home/rancher/ssl/server.key
+
       '''
 
       stage 'Reload nginx'
